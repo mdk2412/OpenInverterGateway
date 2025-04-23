727b5ad96ab235281a7e6025137d1d7090b1a37b
NeuDLi <dirk-mueller@posteo.de>
2025-04-23 21:59:35 +0200

    Update Index.h

--- SRC/ShineWiFi-ModBus/Index.h
+++ SRC/ShineWiFi-ModBus/Index.h
@@ -1,14 +1,11 @@
 #pragma once
-
 const char MAIN_page[] PROGMEM = R"=====(
 <!DOCTYPE HTML>
 <html lang="en">
-
 <head>
     <meta charset='utf-8'>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Growatt Inverter</title>
-    
+    <title>Growatt Inverter</title>    
     <style>
         body {
             min-width: 310px;
@@ -17,22 +14,18 @@ const char MAIN_page[] PROGMEM = R"=====(
             margin: 0 auto;
             font-family: Arial;
         }
-
         h2 {
             font-size: 2.5rem;
             text-align: center;
         }
-
         div {
             font-size: 1rem;
             padding: 10px 0px 10px 0px;
         }
-
         .linkButtonBar {
             text-align: center;
             padding: 20px 0px 20px 0px;
         }
-
         .linkButton {
             -webkit-border-radius: 4px;
             -moz-border-radius: 4px;
@@ -51,7 +44,6 @@ const char MAIN_page[] PROGMEM = R"=====(
             font-size: .8rem;
             text-align: center;
         }
-
         .yellow {
             border: solid 1px rgb(202, 189, 95);
             text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.4);
@@ -60,7 +52,6 @@ const char MAIN_page[] PROGMEM = R"=====(
             box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);
             background:rgb(185, 178, 43);
         }
-
         .red {
             border: solid 1px rgb(202, 95, 95);
             text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.4);
@@ -71,34 +62,24 @@ const char MAIN_page[] PROGMEM = R"=====(
         }
     </style>
 </head>
-
 <body>
     <h2>Growatt MIN TL-XH</h2>
-
     <div class="linkButtonBar">
         <a href="./status" class="linkButton">Json</a>
         <a href="./uiStatus" class="linkButton">UI Json</a>
         <a href="./metrics" class="linkButton">Metrics</a>
         <a href="./debug" class="linkButton">Log</a>
-        <a onClick="return confirm('Starting config AP will disconnect you from the device. Are you sure?');" href="./startAp" class="linkButton yellow">Start Config AP</a>
-        <a onClick="return confirm('This will reboot the Wifi Stick. Are you sure?');" href="./reboot" class="linkButton yellow">Reboot</a>
+        <a onClick="return confirm('Start Config AP?');" href="./startAp" class="linkButton yellow">Start Config AP</a>
+        <a onClick="return confirm('Reboot?');" href="./reboot" class="linkButton yellow">Reboot</a>
         <a href="./postCommunicationModbus" class="linkButton red">RW Modbus</a>
     </div>
-
     <div id="DataContainer"></div>
-
 </body>
 </html>
 )=====";
-
 const char SendPostSite_page[] PROGMEM = R"=====(
-<!DOCTYPE HTML><html>
-<!-- Rui Santos - Complete project details at https://RandomNerdTutorials.com
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files.
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software. -->
+<!DOCTYPE HTML>
+<html lang="en">
 <head>
   <meta charset='utf-8'>
   <meta name="viewport" content="width=device-width, initial-scale=1">
